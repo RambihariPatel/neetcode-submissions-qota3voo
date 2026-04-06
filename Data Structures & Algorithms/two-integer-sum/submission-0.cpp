bc10306ad0de8e15badfd10c7e9824092280120a
RambihariPatel <rambiharipatel175@gmail.com>
2026-04-06 21:21:20 +0530

    Bulk sync: 12 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        int left = 0, right = n - 1;
+
+        for(int i = 0; i< n; i++){
+            int needed = target - nums[i];
+
+
+            if (mp.find(needed) != mp.end()){
+                return {mp[needed], i};
+            }
+
+            mp[nums[i]] = i;
+        }
+        return {};
+    }
+};
